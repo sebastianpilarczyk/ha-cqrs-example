f7582bcf8f90581df13c098f9584a6161bd52cb6
Sebastian Pilarczyk <sebastian.pilarczyk@gmail.com>
2021-07-28 12:43:07 +0200

    Init project

---
+++ src/main/java/com/javnie/example/hacqrsexample/HaCqrsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javnie.example.hacqrsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaCqrsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaCqrsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javnie/example/hacqrsexample/HaCqrsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javnie.example.hacqrsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaCqrsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
